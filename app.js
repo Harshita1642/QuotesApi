9dcbc3501d9681cacb92a9186c383ca06b07a5d4
harshita <harshita19062004@gmail.com>
2024-03-25 13:58:01 +0530

    implemented cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express=require("express");
+const cors=require("cors");
 const app=express();
 const PORT=process.env.PORT ||  5000;
+
+app.use(cors());
 
 const quotes_routes=require("./routes/quotes");
 app.get("/",(req,res)=>{
     res.redirect("/api/quotes");
 })
-
 app.use("/api/quotes",quotes_routes);
 const start= async ()=>{
     try{
